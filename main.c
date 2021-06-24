6c433eaf3e06c536b05b8e4ad9dd673861d81877
Rabeeh T A <rabeehchemnad@gmail.com>
2021-06-25 00:13:43 +0530

    insert a new node to linked list

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
+// linked-list node.
+typedef struct node
+{
+    int data;
+    struct node *next;
+} node;
+
+node *head = NULL; // this is the head of the linked list.
+
+// function prototypes
 void main_menu();
+void insert_a_node();
+void free_memory();
+void print_linked_list();
 
 int main()
 {
@@ -17,15 +31,16 @@ int main()
         switch (choice)
         {
         case 1:
-            printf("do the inserting part\n");
+            insert_a_node();
             break;
         case 2:
             printf("do the Deleting part\n");
             break;
         case 3:
-            printf("do the Listing the whole linked list part\n");
+            print_linked_list();
             break;
         case 4:
+            free_memory();
             quit = true; // exit out of the while loop and also the entire program.
             break;
         default:
@@ -44,8 +59,64 @@ void main_menu()
     printf("\n");
     printf("1 -> Insert\n");
     printf("2 -> Delete\n");
-    printf("3 -> View\n");
+    printf("3 -> View Linked List\n");
     printf("4 -> Quit\n");
     printf("\n");
     printf("Enter the Number from the menu ");
+}
+
+// inserting a node to the linked list. Insert to beginning, Ending or to any specific index.
+void insert_a_node()
+{
+    int choice = 0;
+    do
+    {
+
+        printf("\n-----Inserting Menu-----\n");
+        printf("1 -> Insert to Beginning\n");
+        printf("2 -> Insert to Ending\n");
+        printf("3 -> Insert by Index Value\n");
+        printf("4 -> Return to Main Menu\n");
+        printf("\n");
+        printf("Enter the Number from the menu ");
+        scanf("%d", &choice);
+
+        if (choice == 4)
+        {
+            return;
+        }
+
+    } while (choice > 5 || choice == 0); // 0 and anything above 4 is invalid
+
+    // make a node and get the value for the in to the node
+    node *n;
+    n = malloc(sizeof(node));
+
+    // get the value to store in linked list
+    int data; // user input to store in linked list
+    printf("Enter the data to store in linked list (datatype int) ");
+    scanf("%d", &data);
+
+    // put the data to the node
+    n->data = data;
+    n->next = NULL;
+
+    // now insert this to the linked list
+    if (choice == 1) // insert to the begging of the list
+    {
+        node *temp = head; // store the starting part
+        head = n;          // head is the new node
+        n->next = temp;    // then the list starts after the new node
+    }
+    else if (choice == 2) // insert to the begging of the list
+    {
+        node *i = head;
+        for (i != NULL; i = i->next;)
+        {
+            continue;
+        }
+        // now i is the last node
+        i->next = n;
+    }
+    free(n);
 }
